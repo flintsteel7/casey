8f78e087d35e1db6ed7545cb7037b249c6d968ae
Peter Turner <peter.turner@lifeway.com>
2018-08-30 15:44:46 -0700

    scaffold public functions

--- lib/casey.ex
+++ lib/casey.ex
@@ -15,4 +15,28 @@ defmodule Casey do
   def hello do
     :world
   end
+
+  def all_caps input, mode do
+    String.upcase(input, mode)
+  end
+
+  def all_lower input, mode do
+    String.downcase(input, mode)
+  end
+
+  def cap_words input do
+    input
+  end
+
+  def cap_sentences input do
+    input
+  end
+
+  def cap_lines input do
+    input
+  end
+
+  def title_case input do
+    input
+  end
 end

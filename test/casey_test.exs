931c671b784eb86e1255d74c4bfebb23d52fdd14
Peter Turner <peter.turner@lifeway.com>
2018-08-30 17:33:02 -0700

    add basic tests

--- test/casey_test.exs
+++ test/casey_test.exs
@@ -5,4 +5,38 @@ defmodule CaseyTest do
   test "greets the world" do
     assert Casey.hello() == :world
   end
+
+  test "capitalizes every letter" do
+    assert Casey.all_caps("Upper case all the things!", :default) == "UPPER CASE ALL THE THINGS!"
+  end
+
+  test "lower-cases every letter" do
+    assert Casey.all_lower("Bring it all Down, Boys!", :default) == "bring it all down, boys!"
+  end
+
+  test "capitalize every word" do
+    assert Casey.cap_words("I'd like all these words to be capitalized, please.") == "I'd Like All These Words To Be Capitalized, Please."
+  end
+
+  test "capitalize each sentence" do
+    assert Casey.cap_sentences("here is Red. Red is a dog. see Red run. go Red, go!") == "Here is Red. Red is a dog. See Red run. Go Red, go!"
+  end
+
+  test "capitalize each line" do
+    assert Casey.cap_lines("""
+let each of these lines
+come back capitalized
+or something isn't working
+correctly
+""") == """
+Let each of these lines
+Come back capitalized
+Or something isn't working
+Correctly
+"""
+  end
+
+  test "properly capitalize title" do
+    assert Casey.title_case("the hound of the Baskervilles") === "The Hound of the Baskervilles"
+  end
 end
